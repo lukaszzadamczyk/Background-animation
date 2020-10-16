7174e80a91a63a5d4b81f0da2d87d4c3948d2aa6
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2020-10-16 10:12:34 +0100

    Updates

---
+++ precache-manifest.d66f6594c77960b75447d2145f295845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a308d3fd01fcb8fbe3b4a9ecf78820",
+    "url": "/Background-animation/index.html"
+  },
+  {
+    "revision": "5a0e0c8aecec3a5d6d3a",
+    "url": "/Background-animation/static/css/main.f9616236.chunk.css"
+  },
+  {
+    "revision": "9751b34721419c22209b",
+    "url": "/Background-animation/static/js/2.4eb046d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Background-animation/static/js/2.4eb046d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0e0c8aecec3a5d6d3a",
+    "url": "/Background-animation/static/js/main.50eb124c.chunk.js"
+  },
+  {
+    "revision": "fe6e8bf8359b554fcccd",
+    "url": "/Background-animation/static/js/runtime-main.5b44e79b.js"
+  }
+]);
